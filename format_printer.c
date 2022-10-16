f850ea27533e32c7d34c55c238d2d61c2d98cf49
Taiwo Babalola <babalolataiwop@gmail.com>
2022-10-17 00:53:16 +0100

    correct description for function

--- format_printer.c
+++ format_printer.c
@@ -4,7 +4,7 @@
  * format_specifier - performs the function choice and sets the format
  * appropriately
  * @format: format string that that takes in all chars and specifiers
- * @func_list: has type format_spec; array of struct members
+ * @f_list: has type format_spec; array of struct members
  * i.e specifiers and functions
  * @args_list: has type va_list; holds the info about format and va_start
  * Return: amount of characters printed
