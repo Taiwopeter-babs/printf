c9edd760f4a76d4f4cbe1d175b953efce6156b11
Adeola <adeolapraise02@gmail.com>
2022-10-19 02:03:25 +0000

    Add More Test Cases

--- tests/test_int.c
+++ tests/test_int.c
@@ -1,5 +1,6 @@
 #include "../main.h"
 #include <stdio.h>
+#include <limits.h>
 
 
 int main(void)
@@ -7,12 +8,15 @@ int main(void)
 	int a;
 	int b;
 	unsigned int ui;
+	unsigned int c;
 
 	a = 345;
 	b = 5;
-	ui = (unsigned int)INT_MAX + 1024;
+	ui = 567;
+	c = 98;
 
-	_printf(":[%d], [%i]\n", a, b);
+	_printf("integer:[%d], [%i]\n", a, b);
+	_printf("Binary: [%b]\n", c);
 	_printf("Unsigned octal:[%o]\n", ui);
 	printf("Unsigned octal:[%o]\n", ui);
 	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
