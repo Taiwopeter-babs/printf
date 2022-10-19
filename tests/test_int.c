a39c3680e8cf7d765a38460d5683ab3759fb6962
Adeola <adeolapraise02@gmail.com>
2022-10-19 00:19:37 +0000

    Modify Test File

--- tests/test_int.c
+++ tests/test_int.c
@@ -6,10 +6,17 @@ int main(void)
 {
 	int a;
 	int b;
+	unsigned int ui;
 
 	a = 345;
 	b = 5;
+	ui = (unsigned int)INT_MAX + 1024;
 
 	_printf(":[%d], [%i]\n", a, b);
+	_printf("Unsigned octal:[%o]\n", ui);
+	printf("Unsigned octal:[%o]\n", ui);
+	_printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+	printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+
 	return (0);
 }
